9cce371641011899779eb58657889cbd1435461f
Jay Offerdahl <jaysanco@gmail.com>
2017-04-20 22:45:16 -0500

    Project 3 submitted.

---
+++ project3/README.txt
@@ -0,0 +1,17 @@
+#####################################################################
+#
+#   EECS 662:   Assignment 3
+#   Author:     Jay Offerdahl
+#   Date:       Thu Apr 20, 2017
+#
+#####################################################################
+
+Hey Dr. Perry,
+
+Here's my submission for assignment 3. I used the let notation for
+everything because I have finally decided that it is the best. See
+you in class!
+
+Thanks,
+
+Jay
--- project3/proj3Utils.hs
+++ project3/proj3Utils.hs
@@ -26,9 +26,11 @@ import ParserUtils
 -- Author: Perry Alexander
 -- Date: 6 April 2017
 --
+-- Edited By: Jay Offerdahl
+-- Date: 20 April 2017
+--
 
 -- CFAE AST Definition
-
 data CFAE where
   Num :: Int -> CFAE
   Plus :: CFAE -> CFAE -> CFAE
@@ -38,11 +40,10 @@ data CFAE where
   Lambda :: String -> CFAE -> CFAE
   App :: CFAE -> CFAE -> CFAE
   Id :: String -> CFAE
-  If :: CFAE -> CFAE -> CFAE -> CFAE
+  If0 :: CFAE -> CFAE -> CFAE -> CFAE
   deriving (Show,Eq)
 
 -- Parser
-
 expr :: Parser CFAE
 expr = buildExpressionParser opTable term
 
@@ -59,7 +60,7 @@ numExpr = do i <- integer lexer
 identExpr :: Parser CFAE
 identExpr = do i <- identifier lexer
                return (Id i)
-              
+
 lambdaExpr :: Parser CFAE
 lambdaExpr = do reserved lexer "lambda"
                 i <- identifier lexer
@@ -74,33 +75,97 @@ appExpr = do reserved lexer "app"
              return (App e1 e2)
 
 ifExpr :: Parser CFAE
-ifExpr = do reserved lexer "if"
+ifExpr = do reserved lexer "if0"
             c <- expr
             reserved lexer "then"
             t <- expr
             reserved lexer "else"
             e <- expr
-            return (If c t e)
-            
-             
+            return (If0 c t e)
+
 term = parens lexer expr
        <|> numExpr
        <|> identExpr
        <|> ifExpr
        <|> lambdaExpr
        <|> appExpr
-             
--- Parser invocation
 
+-- Parser invocation
 parseCFAE = parseString expr
 
 parseCFAEFile = parseFile expr
 
 
--- CFBAE Parser
+-- Exercise 1
+type EnvD = [(String, CFAE)]
 
+evalDynCFAE :: EnvD -> CFAE -> CFAE
+evalDynCFAE env (Num x) = (Num x)
+evalDynCFAE env (Plus l r) = let (Num l') = (evalDynCFAE env l)
+                                 (Num r') = (evalDynCFAE env r)
+                             in (Num (l' + r'))
+evalDynCFAE env (Minus l r) = let (Num l') = (evalDynCFAE env l)
+                                  (Num r') = (evalDynCFAE env r)
+                              in (Num (l' - r'))
+evalDynCFAE env (Mult l r) = let (Num l') = (evalDynCFAE env l)
+                                 (Num r') = (evalDynCFAE env r)
+                             in (Num (l' * r'))
+evalDynCFAE env (Div l r) = let (Num l') = (evalDynCFAE env l)
+                                (Num r') = (evalDynCFAE env r)
+                            in (Num (div l' r'))
+evalDynCFAE env (Lambda i b) = (Lambda i b)
+evalDynCFAE env (App f a) = let (Lambda i b) = (evalDynCFAE env f)
+                                a' = (evalDynCFAE env a)
+                            in (evalDynCFAE ((i, a'):env) b)
+evalDynCFAE env (Id id) = case (lookup id env) of
+                               Just x -> x
+                               Nothing -> error "Undefined id."
+evalDynCFAE env (If0 c t e) = let (Num c') = (evalDynCFAE env c)
+                              in if c' == 0 then (evalDynCFAE env t) else (evalDynCFAE env e)
+
+interpDynCFAE :: String -> CFAE
+interpDynCFAE input = let parseString = (parseCFAE input)
+                      in (evalDynCFAE [] parseString)
+
+
+-- Exercise 2
+data CFAEValue where
+  NumV :: Int -> CFAEValue
+  ClosureV :: String -> CFAE -> EnvS -> CFAEValue
+  deriving (Show,Eq)
+
+type EnvS = [(String, CFAEValue)]
+
+evalStatCFAE :: EnvS -> CFAE -> CFAEValue
+evalStatCFAE env (Num x) = (NumV x)
+evalStatCFAE env (Plus l r) = let (NumV l') = (evalStatCFAE env l)
+                                  (NumV r') = (evalStatCFAE env r)
+                              in (NumV (l' + r'))
+evalStatCFAE env (Minus l r) = let (NumV l') = (evalStatCFAE env l)
+                                   (NumV r') = (evalStatCFAE env r)
+                               in (NumV (l' - r'))
+evalStatCFAE env (Mult l r) = let (NumV l') = (evalStatCFAE env l)
+                                  (NumV r') = (evalStatCFAE env r)
+                              in (NumV (l' * r'))
+evalStatCFAE env (Div l r) = let (NumV l') = (evalStatCFAE env l)
+                                 (NumV r') = (evalStatCFAE env r)
+                             in (NumV (div l' r'))
+evalStatCFAE env (Lambda i b) = (ClosureV i b env)
+evalStatCFAE env (App f a) =  let (ClosureV i b e) = (evalStatCFAE env f)
+                                  a' = (evalStatCFAE env a)
+                              in evalStatCFAE ((i, a'):e) b
+evalStatCFAE env (Id id) = case (lookup id env) of
+                             Just x -> x
+                             Nothing -> error "Undefined id."
+evalStatCFAE env (If0 c t e) = let (NumV c') = (evalStatCFAE env c)
+                               in if c' == 0 then (evalStatCFAE env t) else (evalStatCFAE env e)
+
+interpStatCFAE input = let parseString = (parseCFAE input)
+                       in (evalStatCFAE [] parseString)
+
+
+-- Exercise 3
 -- CFBAE AST Definition
-
 data CFBAE where
   NumX :: Int -> CFBAE
   PlusX :: CFBAE -> CFBAE -> CFBAE
@@ -111,11 +176,12 @@ data CFBAE where
   LambdaX :: String -> CFBAE -> CFBAE
   AppX :: CFBAE -> CFBAE -> CFBAE
   IdX :: String -> CFBAE
-  IfX :: CFBAE -> CFBAE -> CFBAE -> CFBAE
+  If0X :: CFBAE -> CFBAE -> CFBAE -> CFBAE
+  IncX :: CFBAE -> CFBAE
+  DecX :: CFBAE -> CFBAE
   deriving (Show,Eq)
 
 -- Parser
-
 exprX :: Parser CFBAE
 exprX = buildExpressionParser opTableX termX
 
@@ -141,7 +207,7 @@ bindExprX = do reserved lexer "bind"
                reserved lexer "in"
                e <- exprX
                return (BindX i v e)
-              
+
 lambdaExprX :: Parser CFBAE
 lambdaExprX = do reserved lexer "lambda"
                  i <- identifier lexer
@@ -156,15 +222,24 @@ appExprX = do reserved lexer "app"
               return (AppX e1 e2)
 
 ifExprX :: Parser CFBAE
-ifExprX = do reserved lexer "if"
+ifExprX = do reserved lexer "if0"
              c <- exprX
              reserved lexer "then"
              t <- exprX
              reserved lexer "else"
              e <- exprX
-             return (IfX c t e)
-            
-             
+             return (If0X c t e)
+
+incExprX :: Parser CFBAE
+incExprX = do reserved lexer "inc"
+              x <- exprX
+              return (IncX x)
+
+decExprX :: Parser CFBAE
+decExprX = do reserved lexer "dec"
+              x <- exprX
+              return (DecX x)
+
 termX = parens lexer exprX
        <|> numExprX
        <|> identExprX
@@ -172,10 +247,28 @@ termX = parens lexer exprX
        <|> ifExprX
        <|> lambdaExprX
        <|> appExprX
-             
--- Parser invocation
+       <|> incExprX
+       <|> decExprX
 
+-- Parser invocation
 parseCFBAE = parseString exprX
 
 parseCFBAEFile = parseFile exprX
 
+elabCFBAE :: CFBAE -> CFAE
+elabCFBAE (NumX x) = (Num x)
+elabCFBAE (PlusX l r) = (Plus (elabCFBAE l) (elabCFBAE r))
+elabCFBAE (MinusX l r) = (Minus (elabCFBAE l) (elabCFBAE r))
+elabCFBAE (MultX l r) = (Mult (elabCFBAE l) (elabCFBAE r))
+elabCFBAE (DivX l r) = (Div (elabCFBAE l) (elabCFBAE r))
+elabCFBAE (BindX i v b) = (App (Lambda i (elabCFBAE b)) (elabCFBAE v))
+elabCFBAE (LambdaX i b) = (Lambda i (elabCFBAE b))
+elabCFBAE (AppX f a) = (App (elabCFBAE f) (elabCFBAE a))
+elabCFBAE (IdX id) = (Id id)
+elabCFBAE (If0X c t e) = (If0 (elabCFBAE c) (elabCFBAE t) (elabCFBAE e))
+elabCFBAE (IncX x) = (Plus (elabCFBAE x) (Num 1))
+elabCFBAE (DecX x) = (Minus (elabCFBAE x) (Num 1))
+
+evalCFBAE env = evalStatCFAE env . elabCFBAE
+
+interpCFBAE = evalCFBAE [] . parseCFBAE
